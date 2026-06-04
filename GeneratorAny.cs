265c7329f2d6da75a7ca84698720372e0906d039
chickentuna <chickentuna@hotmail.com>
2026-06-05 00:03:50 +0200

    wip

--- GeneratorAny.cs
+++ GeneratorAny.cs
@@ -12,165 +12,169 @@ using UnityEngine.Events;
 public class GeneratorAny : ProcessingBlock
 {
 
-	public override int actionPhaseOrder => 1001;
+    public override int actionPhaseOrder => 1001;
 
-	private const int toggleDuration = 4;
+    private const int toggleDuration = 4;
 
-	public const int frequency = 2;
+    public const int frequency = 2;
 
-	// are those even used?
+    // are those even used?
 
-	public GeneratorAny()
-	{
+    public BlockVisuals blockVisuals;
 
-	}
-
-	public override void ExecuteActionPhase()
-	{
-		if (!UpdateProcess())
-		{
-			return;
-		}
-		base.outputBlocks.Clear();
-		// found in other controllers:
-		// if ((AbstractSingleton<TimeManager>.Instance.stepIndex) % 2 == 1)
-		// if ((AbstractSingleton<TimeManager>.Instance.stepIndex) % 4 == 1)
-
-		var bytes = File.ReadAllBytes("BepInEx/plugins/FakutoriCustom/generator fx any.png");
-        var myTex = new Texture2D(2, 2);
-        myTex.LoadImage(bytes);
-		myTex.name = "generator fx any";
-
-		if ((AbstractSingleton<TimeManager>.Instance.stepIndex + 32) % 16 == 0)
-		{
-			// var renderers = UnityEngine.Object.FindObjectsOfType<Renderer>();	
-
-			// foreach (var r in renderers)
-			// {
-			// 	// Plugin.Logger.LogInfo($"Renderer: {r.name} ({r.GetType().Name})");
-
-			// 	foreach (var m in r.materials)
-			// 	{
-			// 		if (m != null)
-			// 		{
-			// 			// Plugin.Logger.LogInfo($"  Mat: {m.name} | Shader: {m.shader?.name}");
-			// 			var mpb = new MaterialPropertyBlock();
-
-			// 			r.GetPropertyBlock(mpb);
-			// 			var tex = mpb.GetTexture("_FXTexture");
-			// 			if (tex != null && tex.name.StartsWith("generator fx"))
-			// 			{
-			// 				var t1 = mpb.GetTexture("_FXTexture")?.name ?? "null";
-			// 				mpb.SetTexture("_FXTexture", myTex);
-			// 				var t2 = mpb.GetTexture("_FXTexture")?.name ?? "null";
-			// 				Plugin.Logger.LogInfo($"Updated _FXTexture for {r.name} from {t1} to {t2}");
-			// 			}
-			// 		}
-			// 	}
-			// }
-
-			GridCell cellInDirection = base.gridCell.GetCellInDirection(base.direction);
-			if (cellInDirection.IsCellFree(base.layer))
-			{
-				BlockData GeneratedBlock = GetRandomUnlockedBlock();
-				int num = (base.stepsDuration = 4);
-				base.stepsRemaining = num;
-				base.outputBlocks = new List<ElementBlock> { AbstractSingleton<BlocksManager>.Instance.SpawnBlock(cellInDirection, GeneratedBlock, 4, ignoreBlockOnPosition: false, AbstractSingleton<BlocksManager>.Instance.GetNewBlockStatus(GeneratedBlock)) as ElementBlock };
-				base.outputBlocks[0].onInterruptAction.AddListener(InterruptAction);
-				UnityEvent<BlockAction> onAction = base.OnAction;
-				Block[] array = base.outputBlocks.ToArray();
-				onAction.Invoke(new BlockAction(BlockActionType.Success, 4, 0, null, array));
-			}
-			else
-			{
-				base.OnAction.Invoke(new BlockAction(BlockActionType.Failure, 0));
-			}
-		}
-		// Plugin.Logger.LogInfo("End of GeneratorAny ExecuteActionPhase");
-	}
-
-	private BlockData GetRandomUnlockedBlock()
-	{
-		// Initialize with the four ElementBlocks that have UnlockedByDefault set to true
-		BlocksLibrary blocksLibrary = Resources.FindObjectsOfTypeAll<BlocksLibrary>()[0];
-		var ElementBlocksField = AccessTools.Field(typeof(BlocksLibrary), "ElementBlocks");
-		BlockData[] elementBlocks = (BlockData[])ElementBlocksField.GetValue(blocksLibrary);
-		List<BlockData> list = elementBlocks
-			.Where(block => block.unlockedByDefault)
-			.Where(block => block.category && block.category.showInCompendium)
-		.ToList();
-
-		// Triple the odds:
-		list.AddRange(list);
-		list.AddRange(list);
-
-		// Add in any block with completed challenges
-		ProgressManager progressManager = AbstractSingleton<ProgressManager>.Instance;
-		list.AddRange(
-			elementBlocks
-			.Where(block => !block.unlockedByDefault)
-			.Where(block => progressManager.GetProgress(block).isChallengeCompleted)
-			.ToList()
-		);
-
-		return list[UnityEngine.Random.Range(0, list.Count)];
-	}
-
-	protected override List<ElementBlock> GetOutputBlocks()
-	{
-		ElementBlock elementBlock = base.gridCell?.GetElementBlockInNeighbor(base.direction);
-		if (elementBlock != null)
-		{
-			return new List<ElementBlock> { elementBlock };
-		}
-		return new List<ElementBlock>();
-	}
-
-	public override SerializedBlock SerializeBlock(bool saveAsBlueprint)
-	{
-		return new SerializedProcessingBlock(base.SerializeBlock(saveAsBlueprint))
-		{
-			Duration = ((!saveAsBlueprint) ? base.stepsDuration : 0),
-			ElapsedSteps = ((!saveAsBlueprint) ? (4 - base.stepsRemaining) : 0)
-		};
-	}
-
-	public override void DeserializeBlock(SerializedBlock block, BlockData data)
-	{
-		base.DeserializeBlock(block, data);
-		SerializedProcessingBlock serializedProcessingBlock = block as SerializedProcessingBlock;
-		base.stepsDuration = serializedProcessingBlock.Duration;
-		base.stepsRemaining = base.stepsDuration - serializedProcessingBlock.ElapsedSteps;
-		if (base.stepsRemaining > 0)
-		{
-			base.outputBlocks = GetOutputBlocks();
-			if (base.outputBlocks.Count == 1)
-			{
-				base.outputBlocks[0].onInterruptAction.AddListener(InterruptAction);
-				UnityEvent<BlockAction> onAction = base.OnAction;
-				int num = base.stepsDuration;
-				int num2 = 4 - base.stepsRemaining;
-				Block[] array = base.outputBlocks.ToArray();
-				onAction.Invoke(new BlockAction(BlockActionType.Success, num, num2, null, array));
-			}
-		}
-	}
-
-	public override void DeserializeBlockOnClone(SerializedBlock block, BlockData data)
-	{
-		base.DeserializeBlockOnClone(block, data);
-		int num = (base.stepsRemaining = 0);
-		base.stepsDuration = num;
-	}
-
-	public override Block GetNewInstance()
-	{
-		return new GeneratorAny();
-	}
-
-    public override void Init(GridCell gridCell, Block originalBlock, BlockStatus status, int duration, bool fromSave)
+    public GeneratorAny()
     {
-        base.Init(gridCell, originalBlock, status, duration, fromSave);
 
+    }
+
+    private void ResetIcon()
+    {
+        try 
+        {
+            var go = blockVisuals.transform.Find("Visuals").Find("Block");
+            var ren = go.GetComponent<SpriteRenderer>();
+            var mpb = new MaterialPropertyBlock();
+            ren.GetPropertyBlock(mpb);
+            mpb.SetTexture("_FXTexture", Plugin.GeneratorAnyFxTexture);
+            ren.SetPropertyBlock(mpb);
+        } catch (Exception e) 
+        {
+            // oops
+        }
+        // Why no working either?
+    }
+
+    public override void ExecuteActionPhase()
+    {
+        if (!UpdateProcess())
+        {
+            return;
+        }
+        base.outputBlocks.Clear();
+
+        // if ((AbstractSingleton<TimeManager>.Instance.stepIndex) % 2 == 1)
+        // if ((AbstractSingleton<TimeManager>.Instance.stepIndex) % 4 == 1)
+        // if ((AbstractSingleton<TimeManager>.Instance.stepIndex + 32) % 16 == 0)
+        if (true)
+        {
+            ResetIcon();
+            GridCell cellInDirection = base.gridCell.GetCellInDirection(base.direction);
+            if (cellInDirection.IsCellFree(base.layer))
+            {
+                BlockData GeneratedBlock = GetRandomUnlockedBlock();
+                int num = (base.stepsDuration = 4);
+                base.stepsRemaining = num;
+                base.outputBlocks = new List<ElementBlock> { AbstractSingleton<BlocksManager>.Instance.SpawnBlock(cellInDirection, GeneratedBlock, 4, ignoreBlockOnPosition: false, AbstractSingleton<BlocksManager>.Instance.GetNewBlockStatus(GeneratedBlock)) as ElementBlock };
+                base.outputBlocks[0].onInterruptAction.AddListener(InterruptAction);
+                UnityEvent<BlockAction> onAction = base.OnAction;
+                Block[] array = base.outputBlocks.ToArray();
+                onAction.Invoke(new BlockAction(BlockActionType.Success, 4, 0, null, array));
+            }
+            else
+            {
+                base.OnAction.Invoke(new BlockAction(BlockActionType.Failure, 0));
+            }
+        }
+        // Plugin.Logger.LogInfo("End of GeneratorAny ExecuteActionPhase");
+    }
+
+    private BlockData GetRandomUnlockedBlock()
+    {
+        // Initialize with the four ElementBlocks that have UnlockedByDefault set to true
+        BlocksLibrary blocksLibrary = Resources.FindObjectsOfTypeAll<BlocksLibrary>()[0];
+        var ElementBlocksField = AccessTools.Field(typeof(BlocksLibrary), "ElementBlocks");
+        BlockData[] elementBlocks = (BlockData[])ElementBlocksField.GetValue(blocksLibrary);
+        List<BlockData> list = elementBlocks
+            .Where(block => block.unlockedByDefault)
+            .Where(block => block.category && block.category.showInCompendium)
+        .ToList();
+
+        // Quadruple the odds:
+        list.AddRange(list);
+        list.AddRange(list);
+
+        // Add in any block with completed challenges
+        ProgressManager progressManager = AbstractSingleton<ProgressManager>.Instance;
+        list.AddRange(
+            elementBlocks
+            .Where(block => !block.unlockedByDefault)
+            .Where(block => progressManager.GetProgress(block).isChallengeCompleted)
+            .ToList()
+        );
+
+        return list[UnityEngine.Random.Range(0, list.Count)];
+    }
+
+    protected override List<ElementBlock> GetOutputBlocks()
+    {
+        ElementBlock elementBlock = base.gridCell?.GetElementBlockInNeighbor(base.direction);
+        if (elementBlock != null)
+        {
+            return new List<ElementBlock> { elementBlock };
+        }
+        return new List<ElementBlock>();
+    }
+
+    public override SerializedBlock SerializeBlock(bool saveAsBlueprint)
+    {
+        return new SerializedProcessingBlock(base.SerializeBlock(saveAsBlueprint))
+        {
+            Duration = ((!saveAsBlueprint) ? base.stepsDuration : 0),
+            ElapsedSteps = ((!saveAsBlueprint) ? (4 - base.stepsRemaining) : 0)
+        };
+    }
+
+    public override void DeserializeBlock(SerializedBlock block, BlockData data)
+    {
+        base.DeserializeBlock(block, data);
+        SerializedProcessingBlock serializedProcessingBlock = block as SerializedProcessingBlock;
+        base.stepsDuration = serializedProcessingBlock.Duration;
+        base.stepsRemaining = base.stepsDuration - serializedProcessingBlock.ElapsedSteps;
+        if (base.stepsRemaining > 0)
+        {
+            base.outputBlocks = GetOutputBlocks();
+            if (base.outputBlocks.Count == 1)
+            {
+                base.outputBlocks[0].onInterruptAction.AddListener(InterruptAction);
+                UnityEvent<BlockAction> onAction = base.OnAction;
+                int num = base.stepsDuration;
+                int num2 = 4 - base.stepsRemaining;
+                Block[] array = base.outputBlocks.ToArray();
+                onAction.Invoke(new BlockAction(BlockActionType.Success, num, num2, null, array));
+            }
+        }
+    }
+
+    public override void DeserializeBlockOnClone(SerializedBlock block, BlockData data)
+    {
+        base.DeserializeBlockOnClone(block, data);
+        int num = (base.stepsRemaining = 0);
+        base.stepsDuration = num;
+    }
+
+    public override Block GetNewInstance()
+    {
+        return new GeneratorAny();
+    }
+
+    public override bool isPermanent
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public override bool canBeSoldForMoney
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override bool Despawn()
+    {
+        return !this.isPermanent && base.Despawn();
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ namespace FakutoriCustom;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-    internal static Texture2D FXTexture;
+    internal static Texture2D GeneratorAnyFxTexture;
 
     private void Awake()
     {
@@ -28,6 +28,31 @@ public class Plugin : BaseUnityPlugin
     }
 }
 
+[HarmonyPatch(typeof(GameplayManager))]
+class GameplayManagerPatch
+{
+    [HarmonyPatch("NewGame")]
+    [HarmonyPostfix]
+    public static void PostNewGame()
+    {
+        Plugin.Logger.LogInfo("GameplayManager.NewGame has been called!");
+        // Place 1 generator any at 0,0
+        var blocksManager = AbstractSingleton<BlocksManager>.Instance;
+        GridCell cell = AbstractSingleton<GridManager>.Instance.GetOrCreateGridCell(new Vector2Int(0, 0));
+        blocksManager.SpawnBlock(
+            cell,
+            blocksManager.blocksLibrary.GetBlockDataById(100),
+             duration: 0,
+             ignoreBlockOnPosition: true,
+             status: null,
+             originalBlock: null,
+             fromSave: false
+        );
+
+        
+    }
+}
+
 [HarmonyPatch(typeof(ProgressManager))]
 class ProgressManagerPatch
 {
@@ -36,6 +61,11 @@ class ProgressManagerPatch
     public static void PostStart()
     {
         Plugin.Logger.LogInfo("ProgressManager.Start has been called!");
+
+        var bytes = File.ReadAllBytes("BepInEx/plugins/FakutoriCustom/generator fx any.png");
+        Plugin.GeneratorAnyFxTexture = new Texture2D(128, 128);
+        Plugin.GeneratorAnyFxTexture.LoadImage(bytes);
+        Plugin.GeneratorAnyFxTexture.name = "generator fx any";
 
         // Create a new Block by duplicating Generator Water.
         BlocksLibrary blocksLibrary = Resources.FindObjectsOfTypeAll<BlocksLibrary>()[0];
@@ -53,25 +83,21 @@ class ProgressManagerPatch
         PrefabField.SetValue(anyGenData, anyGenPrefab);
 
 
-
-        var bytes = File.ReadAllBytes("BepInEx/plugins/FakutoriCustom/generator fx any.png");
-        var tex = new Texture2D(2, 2);
-        tex.LoadImage(bytes);
-        Plugin.FXTexture = tex;
-
-
         // Change stuff in the prefab
         // var symbol = mudPrefab.transform.Find("Symbol").gameObject;
         var block = anyGenPrefab.transform.Find("Visuals").Find("Block").gameObject;
 
 
-        // Change block id and name
+        // Change block id, cost and name
         var BlockIdField = AccessTools.Field(typeof(BlockData), "BlockId");
         BlockIdField.SetValue(anyGenData, 100);
 
         var NameKeyField = AccessTools.Field(typeof(BlockData), "NameKey");
         NameKeyField.SetValue(anyGenData, $"custom_Any Generator");
-       
+
+        var MoneyValueField = AccessTools.Field(typeof(BlockData), "MoneyValue");
+        MoneyValueField.SetValue(anyGenData, 0);
+
 
         // Add block to library
         var MachineBlocksField = AccessTools.Field(typeof(BlocksLibrary), "MachineBlocks");
@@ -85,6 +111,8 @@ class ProgressManagerPatch
     }
 }
 
+
+[HarmonyPatch(typeof(EditModeMenu))]
 class EditModeMenuPatch
 {
 
@@ -92,6 +120,7 @@ class EditModeMenuPatch
     [HarmonyPostfix]
     static void PostFillItems(EditModeMenu __instance)
     {
+        Plugin.Logger.LogInfo("EditModeMenu.FillItems has been called!");
         var currentLayerField = AccessTools.Field(typeof(EditModeMenu), "currentLayer");
         var currentLayer = (EditModeLayer)currentLayerField.GetValue(__instance);
         if (currentLayer != EditModeLayer.Machines)
@@ -103,32 +132,50 @@ class EditModeMenuPatch
         // Game has just filled the menu with the standard icons.
         var menuItemsField = AccessTools.Field(typeof(EditModeMenu), "menuItems");
         var menuItems = (List<SelectMenuItem>)menuItemsField.GetValue(__instance);
-        var blocksLibrary = AbstractSingleton<BlocksManager>.Instance.blocksLibrary;
 
-        var AddItemMethod = AccessTools.Method(typeof(EditModeMenu), "AddItem",
-            new Type[] {
-                    typeof(Sprite),      // uiSprite
-                    typeof(string),      // name
-                    typeof(string),      // price
-                    typeof(bool),        // showShadow
-                    typeof(SelectionTool), // tool (base class of UnlockMachineBlockTool)
-                    typeof(bool),        // isLocked
-                    typeof(int)          // atIndex
-            }
-        );
+        // remove from items the blocks with index 3 to 6, the default generators.
+        UnityEngine.Object.Destroy(menuItems[3].gameObject);
+        UnityEngine.Object.Destroy(menuItems[4].gameObject);
+        UnityEngine.Object.Destroy(menuItems[5].gameObject);
+        UnityEngine.Object.Destroy(menuItems[6].gameObject);
+        menuItems.RemoveAt(3);
+        menuItems.RemoveAt(3);
+        menuItems.RemoveAt(3);
+        menuItems.RemoveAt(3);
+        // Remove last item (the generator)
+        UnityEngine.Object.Destroy(menuItems[menuItems.Count-1].gameObject);
+        menuItems.RemoveAt(menuItems.Count-1);
 
-        // Put new machine block in menu
-        BlockData blockData = blocksLibrary.GetBlockDataById(100);
+        menuItemsField.SetValue(__instance, menuItems);
 
-        AddItemMethod.Invoke(__instance, new object[] {
-            blockData.uiSprite,
-            blockData.blockName,
-            blockData.moneyValue.ToString(),
-            true,
-            new PlaceMachineBlockTool(blockData) as SelectionTool,
-            false,
-            -1
-        });
+        // bool allowCreateAnyBlock = false;
+        // if (allowCreateAnyBlock)
+        // {
+        //     var AddItemMethod = AccessTools.Method(typeof(EditModeMenu), "AddItem",
+        //         new Type[] {
+        //                 typeof(Sprite),      // uiSprite
+        //                 typeof(string),      // name
+        //                 typeof(string),      // price
+        //                 typeof(bool),        // showShadow
+        //                 typeof(SelectionTool), // tool (base class of UnlockMachineBlockTool)
+        //                 typeof(bool),        // isLocked
+        //                 typeof(int)          // atIndex
+        //         }
+        //     );
+
+        //     // Put new machine block in menu
+        //     BlockData blockData = blocksLibrary.GetBlockDataById(100);
+
+        //     AddItemMethod.Invoke(__instance, new object[] {
+        //     blockData.uiSprite,
+        //     blockData.blockName,
+        //     blockData.moneyValue.ToString(),
+        //     true,
+        //     new PlaceMachineBlockTool(blockData) as SelectionTool,
+        //     false,
+        //     -1
+        // });
+        // }
     }
 }
 
@@ -153,6 +200,7 @@ class BlockNamePropertyPatch
 [HarmonyPatch(typeof(BlocksManager))]
 class BlocksManagerPatch
 {
+
     [HarmonyPatch(
         "SpawnBlockVisuals",
         new Type[] { typeof(Block), typeof(GridCell) }
@@ -167,12 +215,15 @@ class BlocksManagerPatch
 
         if (onBlock.blockData.blockId == 100)
         {
+            ((GeneratorAny)onBlock).blockVisuals = blockVisuals;
             var go = blockVisuals.transform.Find("Visuals").Find("Block");
             var ren = go.GetComponent<SpriteRenderer>();
             var mpb = new MaterialPropertyBlock();
             ren.GetPropertyBlock(mpb);
-            mpb.SetTexture("_FXTexture", Plugin.FXTexture);
+            mpb.SetTexture("_FXTexture", Plugin.GeneratorAnyFxTexture);
             ren.SetPropertyBlock(mpb);
+
+            // Why no working?
         }
     }
 }
